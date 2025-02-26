f4845990e91dbe83eccd23ecfb80f79c5b8aad3d
Illya Maznitskiy <comercaleuros@gmail.com>
2025-02-26 13:18:53 +0200

    Add to config.py constant OUTPUT_FILE

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 # Websites to scrape
 WORKUA_URL = "https://work.ua/jobs-python/"
 RABOTAUA_URL = "https://rabota.ua/zapros/python"
-DOUUA_URL = "https://dou.ua/lenta/"
+DOUUA_URL = "https://jobs.dou.ua/vacancies/?category=Python"
+
+# Output file
+OUTPUT_FILE = "output/jobs.csv"
