ea02f338f9e853324aba20ae930aa96e116aae4d
Illya Maznitskiy <comercaleuros@gmail.com>
2025-02-27 15:23:07 +0200

    Update file paths in config.py

--- config.py
+++ config.py
@@ -4,4 +4,7 @@ RABOTAUA_URL = "https://rabota.ua/zapros/python"
 DOUUA_URL = "https://jobs.dou.ua/vacancies/?category=Python"
 
 # Output scraping file path
-OUTPUT_FILE = "scraping/data/jobs.csv"
+SCRAPING_OUTPUT_FILE = "scraping/data/jobs.csv"
+
+# Output analysis file path
+ANALYSIS_OUTPUT_FILE = "analytics/data/tech_counts.csv"
