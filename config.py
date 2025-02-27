fac273517b21ff10e728b2d551181e9495148d6a
Illya Maznitskiy <comercaleuros@gmail.com>
2025-02-27 14:28:48 +0200

    Update output file in config.py for clarity

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ WORKUA_URL = "https://work.ua/jobs-python/"
 RABOTAUA_URL = "https://rabota.ua/zapros/python"
 DOUUA_URL = "https://jobs.dou.ua/vacancies/?category=Python"
 
-# Output file
-OUTPUT_FILE = "output/jobs.csv"
+# Output scraping file path
+OUTPUT_FILE = "scraping/data/jobs.csv"
