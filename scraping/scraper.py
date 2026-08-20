cf946d72990154dfeff1e9f5ebbb8fd94f5c8f3a
Illia <illia.maznitskyi@gmail.com>
2026-08-20 20:05:43 +0200

    Update scraper with logging

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -1,10 +1,11 @@
 import os
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+
 from scraping.job_scraping.spiders.douua import DouuaSpider
-
+from logger import logger
 from config import SCRAPING_OUTPUT_FILE
-
+from utils import log_line_break
 
 os.environ.setdefault(
     "SCRAPY_SETTINGS_MODULE", "scraping.job_scraping.settings"
@@ -12,31 +13,33 @@ os.environ.setdefault(
 
 
 def scrape_jobs():
-    print("\nStarting scraping...\n")
+    log_line_break()
+    logger.info("\nStarting scraping...\n")
 
     if os.path.exists(SCRAPING_OUTPUT_FILE):
-        print(
+        logger.info(
             f"{SCRAPING_OUTPUT_FILE} "
             f"exists. Deleting the file to overwrite it..."
         )
         try:
             os.remove(SCRAPING_OUTPUT_FILE)
         except OSError as e:
-            print(f"Error deleting {SCRAPING_OUTPUT_FILE}: {e}")
+            logger.error(f"Error deleting {SCRAPING_OUTPUT_FILE}: {e}")
             exit(1)
 
     settings = get_project_settings()
     process = CrawlerProcess(settings)
 
     try:
-        print("Scraping Dou.ua started...")
+        logger.info("Scraping Dou.ua started...")
         process.crawl(DouuaSpider)
         process.start()
     except Exception as e:
-        print(f"Error during scraping process: {e}")
+        logger.error(f"Error during scraping process: {e}")
         exit(1)
 
-    print("\nScraping finished.\n")
+    logger.info("\nScraping finished.\n")
+    log_line_break()
 
 
 if __name__ == "__main__":
